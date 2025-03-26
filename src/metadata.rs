60c0dd3c724323b1a05215c8babac5a54f4b14ab
sardusmatt <matteo@matchday.com>
2025-03-26 12:18:04 +0100

    Added simple test to check the parser

--- src/metadata.rs
+++ src/metadata.rs
@@ -96,3 +96,37 @@ impl ECSMetadata {
         &self.metadata.labels.container_name
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json;
+
+    #[tokio::test]
+    async fn test_parse_ecs_metadata() {
+        let json_data = r#"
+        {
+            "DockerId": "2969e5e20eda3af46d590cd7adfed899862bbcce424ae438a51a2a0b0edfcda0",
+            "Image": "939885537497.dkr.ecr.us-east-1.amazonaws.com/streamer:latest-production",
+            "Labels": {
+                "com.amazonaws.ecs.cluster": "production",
+                "com.amazonaws.ecs.container-name": "streamer",
+                "com.amazonaws.ecs.task-arn": "arn:aws:ecs:us-east-1:939885537497:task/production/021447970bce4bd58069f1925cd87bc0",
+                "com.amazonaws.ecs.task-definition-family": "streamer",
+                "com.amazonaws.ecs.task-definition-version": "12"
+            },
+            "Limits": {"CPU": 2, "Memory": 0}
+        }"#;
+
+        let metadata: ECSContainerMetadataV4 = serde_json::from_str(json_data)
+            .expect("Failed to deserialize ECSContainerMetadataV4 JSON");
+
+        assert_eq!(metadata.docker_id, "2969e5e20eda3af46d590cd7adfed899862bbcce424ae438a51a2a0b0edfcda0");
+        assert_eq!(metadata.image, "939885537497.dkr.ecr.us-east-1.amazonaws.com/streamer:latest-production");
+        assert_eq!(metadata.labels.cluster, "production");
+        assert_eq!(metadata.labels.container_name, "streamer");
+        assert_eq!(metadata.labels.task_arn, "arn:aws:ecs:us-east-1:939885537497:task/production/021447970bce4bd58069f1925cd87bc0");
+        assert_eq!(metadata.limits.cpu, 2);
+        assert_eq!(metadata.limits.mem, 0);
+    }
+}
